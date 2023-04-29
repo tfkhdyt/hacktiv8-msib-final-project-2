3ce5dcb6370742e955379c793956d8a5a69e2ef6
tfkhdyt <tfkhdyt@proton.me>
2023-04-29 07:13:07 +0700

    feat(user-service): add method to login

--- service/user_service.go
+++ service/user_service.go
@@ -8,7 +8,7 @@ import (
 
 type UserService interface {
 	Register(payload *dto.RegisterRequest) (*dto.RegisterResponse, errs.MessageErr)
-	// Login(payload *dto.LoginRequest) (*dto.LoginResponse, errs.MessageErr)
+	Login(payload *dto.LoginRequest) (*dto.LoginResponse, errs.MessageErr)
 	// UpdateUser(payload *dto.UpdateUserRequest) (*dto.UpdateUserResponse, errs.MessageErr)
 	// DeleteUser(payload *dto.DeleteUserRequest) (*dto.DeleteUserResponse, errs.MessageErr)
 }
@@ -37,6 +37,28 @@ func (u *userService) Register(payload *dto.RegisterRequest) (*dto.RegisterRespo
 		Username: registeredUser.Username,
 		Email:    registeredUser.Email,
 		Age:      registeredUser.Age,
+	}
+
+	return response, nil
+}
+
+func (u *userService) Login(payload *dto.LoginRequest) (*dto.LoginResponse, errs.MessageErr) {
+	user, err := u.userRepo.GetUserByEmail(payload.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := user.ComparePassword(payload.Password); err != nil {
+		return nil, err
+	}
+
+	token, err2 := user.CreateToken()
+	if err2 != nil {
+		return nil, err2
+	}
+
+	response := &dto.LoginResponse{
+		Token: token,
 	}
 
 	return response, nil
