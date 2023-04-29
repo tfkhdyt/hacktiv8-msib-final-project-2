ca98ecb5d13505999bf149f6d157c4f879e00d49
tfkhdyt <tfkhdyt@proton.me>
2023-04-29 07:12:24 +0700

    feat(user-entity): add method to compare password and create jwt token

--- entity/user.go
+++ entity/user.go
@@ -2,10 +2,16 @@ package entity
 
 import (
 	"hacktiv8-msib-final-project-2/pkg/errs"
+	"log"
+	"os"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
+
+var JWT_SECRET = os.Getenv("JWT_SECRET")
 
 type User struct {
 	gorm.Model
@@ -24,4 +30,28 @@ func (u *User) HashPassword() errs.MessageErr {
 	u.Password = string(hashedPassword)
 
 	return nil
+}
+
+func (u *User) ComparePassword(password string) errs.MessageErr {
+	if err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password)); err != nil {
+		return errs.NewBadRequest("Password is not valid!")
+	}
+
+	return nil
+}
+
+func (u *User) CreateToken() (string, errs.MessageErr) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"userId": u.ID,
+			"exp":    time.Now().Add(1 * time.Hour),
+		})
+
+	signedString, err := token.SignedString([]byte(JWT_SECRET))
+	if err != nil {
+		log.Println("Error:", err.Error())
+		return "", errs.NewInternalServerError("Failed to sign jwt token")
+	}
+
+	return signedString, nil
 }
