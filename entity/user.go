fe6f858643e5b7f2a4af705fb308d4c683672340
tfkhdyt <tfkhdyt@proton.me>
2023-05-01 10:53:59 +0700

    feat(user-entity): add ValidateToken method

--- entity/user.go
+++ entity/user.go
@@ -4,6 +4,7 @@ import (
 	"hacktiv8-msib-final-project-2/pkg/errs"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -54,4 +55,55 @@ func (u *User) CreateToken() (string, errs.MessageErr) {
 	}
 
 	return signedString, nil
+}
+
+func (u *User) ValidateToken(bearerToken string) errs.MessageErr {
+	if isBearer := strings.HasPrefix(bearerToken, "Bearer"); !isBearer {
+		return errs.NewUnauthorized("Token type should be Bearer")
+	}
+
+	splitToken := strings.Fields(bearerToken)
+	if len(splitToken) != 2 {
+		return errs.NewUnauthorized("Token is not valid")
+	}
+
+	tokenString := splitToken[1]
+	token, err := u.ParseToken(tokenString)
+	if err != nil {
+		return err
+	}
+
+	var mapClaims jwt.MapClaims
+
+	if claims, ok := token.Claims.(jwt.MapClaims); !ok || !token.Valid {
+		return errs.NewUnauthorized("Token is not valid")
+	} else {
+		mapClaims = claims
+	}
+
+	return u.bindTokenToUserEntity(mapClaims)
+}
+
+func (u *User) ParseToken(tokenString string) (*jwt.Token, errs.MessageErr) {
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errs.NewUnauthorized("Token method is not valid")
+		}
+		return []byte(JWT_SECRET), nil
+	})
+	if err != nil {
+		return nil, errs.NewUnauthorized("Token is not valid")
+	}
+
+	return token, nil
+}
+
+func (u *User) bindTokenToUserEntity(claim jwt.MapClaims) errs.MessageErr {
+	if id, ok := claim["userId"].(float64); !ok {
+		return errs.NewUnauthorized("Token doesn't contains userId")
+	} else {
+		u.ID = uint(id)
+	}
+
+	return nil
 }
