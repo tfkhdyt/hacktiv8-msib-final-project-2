e0e13cc0ccc8939d65c78b1f7d4fffb38a9d5f28
tfkhdyt <tfkhdyt@proton.me>
2023-05-07 09:36:16 +0700

    feat(photo-dto): add GetAllPhotosResponse struct

--- dto/photo.go
+++ dto/photo.go
@@ -27,3 +27,19 @@ type CreatePhotoResponse struct {
 	UserID    uint      `json:"user_id"`
 	CreatedAt time.Time `json:"created_at"`
 }
+
+type GetAllPhotosResponse struct {
+	ID        uint      `json:"id"`
+	Title     string    `json:"title"`
+	Caption   string    `json:"caption"`
+	PhotoURL  string    `json:"photo_url"`
+	UserID    uint      `json:"user_id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	User      UserData  `json:"user"`
+}
+
+type UserData struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+}
