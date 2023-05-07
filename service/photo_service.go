50a77aba2ff7bd49b1ec756f2f8501b6780622d2
tfkhdyt <tfkhdyt@proton.me>
2023-05-07 09:35:55 +0700

    feat(photo-service): add get all photos method

--- service/photo_service.go
+++ service/photo_service.go
@@ -5,18 +5,21 @@ import (
 	"hacktiv8-msib-final-project-2/entity"
 	"hacktiv8-msib-final-project-2/pkg/errs"
 	"hacktiv8-msib-final-project-2/repository/photo_repository"
+	"hacktiv8-msib-final-project-2/repository/user_repository"
 )
 
 type PhotoService interface {
 	CreatePhoto(user *entity.User, payload *dto.CreatePhotoRequest) (*dto.CreatePhotoResponse, errs.MessageErr)
+	GetAllPhotos() ([]dto.GetAllPhotosResponse, errs.MessageErr)
 }
 
 type photoService struct {
 	photoRepo photo_repository.PhotoRepository
+	userRepo  user_repository.UserRepository
 }
 
-func NewPhotoService(photoRepo photo_repository.PhotoRepository) PhotoService {
-	return &photoService{photoRepo: photoRepo}
+func NewPhotoService(photoRepo photo_repository.PhotoRepository, userRepo user_repository.UserRepository) PhotoService {
+	return &photoService{photoRepo: photoRepo, userRepo: userRepo}
 }
 
 func (p *photoService) CreatePhoto(user *entity.User, payload *dto.CreatePhotoRequest) (*dto.CreatePhotoResponse, errs.MessageErr) {
@@ -34,6 +37,37 @@ func (p *photoService) CreatePhoto(user *entity.User, payload *dto.CreatePhotoRe
 		PhotoURL:  createdPhoto.PhotoURL,
 		UserID:    createdPhoto.UserID,
 		CreatedAt: createdPhoto.CreatedAt,
+	}
+
+	return response, nil
+}
+
+func (p *photoService) GetAllPhotos() ([]dto.GetAllPhotosResponse, errs.MessageErr) {
+	photos, err := p.photoRepo.GetAllPhotos()
+	if err != nil {
+		return nil, err
+	}
+
+	response := []dto.GetAllPhotosResponse{}
+	for _, photo := range photos {
+		user, err := p.userRepo.GetUserByID(photo.UserID)
+		if err != nil {
+			return nil, err
+		}
+
+		response = append(response, dto.GetAllPhotosResponse{
+			ID:        photo.ID,
+			Title:     photo.Title,
+			Caption:   photo.Caption,
+			PhotoURL:  photo.PhotoURL,
+			UserID:    photo.UserID,
+			CreatedAt: photo.CreatedAt,
+			UpdatedAt: photo.UpdatedAt,
+			User: dto.UserData{
+				Email:    user.Email,
+				Username: user.Username,
+			},
+		})
 	}
 
 	return response, nil
