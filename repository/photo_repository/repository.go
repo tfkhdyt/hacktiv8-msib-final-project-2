22ffb569ed557993b99a6718ef6171448e6cb304
tfkhdyt <tfkhdyt@proton.me>
2023-05-08 11:59:53 +0700

    feat(photo-repo): add GetPhotoByID and UpdatePhoto method

--- repository/photo_repository/repository.go
+++ repository/photo_repository/repository.go
@@ -8,6 +8,7 @@ import (
 type PhotoRepository interface {
 	CreatePhoto(user *entity.User, photo *entity.Photo) (*entity.Photo, errs.MessageErr)
 	GetAllPhotos() ([]entity.Photo, errs.MessageErr)
-	// UpdatePhoto(oldPhoto *entity.Photo, newPhoto *entity.Photo) (*entity.Photo, errs.MessageErr)
+	GetPhotoByID(id uint) (*entity.Photo, errs.MessageErr)
+	UpdatePhoto(oldPhoto *entity.Photo, newPhoto *entity.Photo) (*entity.Photo, errs.MessageErr)
 	// DeletePhoto(id uint) errs.MessageErr
 }
